da2fb0928b5549b48a3cd3115dc65a076a3648a2
Samuel Bertolet <sam@bertol.et>
2024-07-26 20:13:22 +0200

    added baseline support packages

---
+++ cmake@3.22.rb
@@ -0,0 +1,79 @@
+class CmakeAT322 < Formula
+  desc "Cross-platform make"
+  homepage "https://www.cmake.org/"
+  version "3.22.1"
+  url "https://github.com/Kitware/CMake/releases/download/v#{version}/cmake-#{version}.tar.gz"
+  sha256 "3c1c478b9650b107d452c5bd545c72e2fad4e37c09b89a1984b9a2f46df6aced"
+  license "BSD-3-Clause"
+  head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
+
+  conflicts_with "cmake", because: "it conflicts with the newer version of cmake"
+
+  # The "latest" release on GitHub has been an unstable version before, and
+  # there have been delays between the creation of a tag and the corresponding
+  # release, so we check the website's downloads page instead.
+  livecheck do
+    url "https://cmake.org/download/"
+    regex(/href=.*?cmake[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  uses_from_macos "ncurses"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --no-system-libs
+      --parallel=#{ENV.make_jobs}
+      --datadir=/share/cmake
+      --docdir=/share/doc/cmake
+      --mandir=/share/man
+      --system-zlib
+      --system-bzip2
+      --system-curl
+    ]
+
+    system "./bootstrap", *args, "--", *std_cmake_args,
+                                      "-DCMake_INSTALL_BASH_COMP_DIR=#{bash_completion}",
+                                      "-DCMake_INSTALL_EMACS_DIR=#{elisp}",
+                                      "-DCMake_BUILD_LTO=ON"
+    system "make"
+    system "make", "install"
+
+    # Symlink the cmake files into HOMEBREW_PREFIX so they're accessible.
+    (lib/"pkgconfig").install_symlink Dir["#{prefix}/lib/pkgconfig/*"]
+
+    # Remove conflicting symlinks if they exist.
+    rm_rf %W[
+      #{bin}/ccmake
+      #{bin}/cmake
+      #{bin}/cpack
+      #{bin}/ctest
+    ]
+
+    # Install versioned and unversioned symlinks.
+    %w[ccmake cmake cpack ctest].each do |prog|
+      bin.install_symlink "#{prefix}/bin/#{prog}" => "#{prog}#{version.major_minor}"
+      bin.install_symlink "#{prefix}/bin/#{prog}" => prog
+    end
+  end
+
+  def caveats
+    <<~EOS
+      This version of CMake conflicts with the upstream cmake.
+      If you are using this, we assume it is because you wish to compile older Qt frameworks.
+      Unversioned and versioned symlinks for cmake, ccmake, cpack, and ctest have been installed into:
+        #{opt_bin}
+    EOS
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write("find_package(Ruby)")
+    system bin/"cmake", "."
+
+    # Verify that the binary symlinks work.
+    assert_predicate bin/"cmake", :exist?
+    assert_predicate bin/"ccmake", :exist?
+    assert_predicate bin/"cpack", :exist?
+    assert_predicate bin/"ctest", :exist?
+  end
+end
---
+++ postgresql@9.6.rb
@@ -0,0 +1,88 @@
+class PostgresqlAT96 < Formula
+  desc "Relational database management system"
+  homepage "https://www.postgresql.org/"
+  version = "9.6.24"
+  url "https://ftp.postgresql.org/pub/source/v#{version}/postgresql-#{version}.tar.bz2"
+  sha256 "aeb7a196be3ebed1a7476ef565f39722187c108dd47da7489be9c4fcae982ace"
+  license "PostgreSQL"
+
+  livecheck do
+    url "https://ftp.postgresql.org/pub/source/"
+    regex(%r{href=["']?v?(9\.6(?:\.\d+)*)/?["' >]}i)
+  end
+
+  keg_only :versioned_formula
+
+  option "with-cassert", "Enable assertion checks (for debugging)"
+  deprecated_option "enable-cassert" => "with-cassert"
+
+  # https://www.postgresql.org/support/versioning/
+  # deprecate! date: "2021-11-11", because: :unsupported
+
+  depends_on "gettext"
+  depends_on "openldap"
+  depends_on "openssl@1.1"
+  depends_on "python@3.9"
+  depends_on "readline"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --enable-dtrace
+      --enable-nls
+      --with-bonjour
+      --with-gssapi
+      --with-ldap
+      --with-libxml
+      --with-libxslt
+      --with-openssl
+      --with-uuid=e2fs
+      --with-pam
+      --with-perl
+      --with-python
+      PYTHON=#{Formula["python@3.9"].opt_bin/"python3"}
+      XML2_CONFIG=:
+    ]
+
+    # Add include and library directories of dependencies, so that
+    # they can be used for compiling extensions.  Superenv does this
+    # when compiling this package, but won't record it for pg_config.
+    deps = %w[gettext openldap openssl@1.1 readline]
+    with_includes = deps.map { |f| Formula[f].opt_include }.join(":")
+    with_libraries = deps.map { |f| Formula[f].opt_lib }.join(":")
+    args << "--with-includes=#{with_includes}"
+    args << "--with-libraries=#{with_libraries}"
+
+    args << "--enable-cassert" if build.with? "cassert"
+
+    extra_version = ""
+    extra_version += "+git" if build.head?
+    extra_version += " (Homebrew petere/postgresql)"
+    args << "--with-extra-version=#{extra_version}"
+
+    ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+
+    system "./configure", *args
+    system "make", "install-world"
+  end
+
+  def caveats
+    <<~EOS
+      To use this PostgreSQL installation, do one or more of the following:
+
+      - Call all programs explicitly with #{opt_prefix}/bin/...
+      - Add #{opt_bin} to your PATH
+      - brew link -f #{name}
+      - Install the postgresql-common package
+
+      To access the man pages, do one or more of the following:
+      - Refer to them by their full path, like `man #{opt_share}/man/man1/psql.1`
+      - Add #{opt_share}/man to your MANPATH
+      - brew link -f #{name}
+    EOS
+  end
+
+  test do
+    system "#{bin}/initdb", "pgdata"
+  end
+end
