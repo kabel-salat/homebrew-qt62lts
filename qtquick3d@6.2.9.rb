0065d9c3ee3a1c16d48fefe4386185e34eacb2f3
Samuel Bertolet <sam@bertol.et>
2024-07-27 17:27:38 +0200

    added more qtquick modules and updated dependencies

--- qtquick3d@6.2.9.rb
+++ qtquick3d@6.2.9.rb
@@ -1,8 +1,8 @@
-class QtquicktimelineAT629 < Formula
-  desc "Qt 6.2.9 LTS Quick Timeline Module"
+class Qtquick3dAT629 < Formula
+  desc "Qt 6.2.9 LTS Quick 3D Module"
   homepage "https://www.qt.io/"
-  url "https://github.com/qt/qtquicktimeline/archive/refs/tags/v6.2.9-lts-lgpl.tar.gz"
-  sha256 "9d82818bbda2e6368cdad466dc3de6ff753e7e8328f5c75b080770fb49348c86"
+  url "https://github.com/qt/qtquick3d/archive/refs/tags/v6.2.9-lts-lgpl.tar.gz"
+  sha256 "8532536965d3081513aba022bf34e11e3afa761666360d3e04fa2af432a82229"
   license all_of: [
     "BSD-3-Clause",
     "GFDL-1.3-no-invariants-only",
@@ -15,7 +15,6 @@ class QtquicktimelineAT629 < Formula
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "qt@6.2.9"
-  depends_on "qtquick3d@6.2.9"
 
   def install
     qt_prefix = Formula["qt@6.2.9"].opt_prefix
@@ -30,7 +29,7 @@ class QtquicktimelineAT629 < Formula
   test do
     (testpath/"test.cpp").write <<~EOS
       #include <QCoreApplication>
-      #include <QtQuickTimeline>
+      #include <QtQuick3D>
       int main(int argc, char *argv[]) {
         QCoreApplication app(argc, argv);
         return 0;
@@ -38,7 +37,7 @@ class QtquicktimelineAT629 < Formula
     EOS
 
     qt_prefix = Formula["qt@6.2.9"].opt_prefix
-    system ENV.cxx, "test.cpp", "-I#{qt_prefix}/include", "-I#{qt_prefix}/include/QtQuickTimeline", "-L#{qt_prefix}/lib", "-lQt6QuickTimeline", "-o", "test"
+    system ENV.cxx, "test.cpp", "-I#{qt_prefix}/include", "-I#{qt_prefix}/include/QtQuick3D", "-L#{qt_prefix}/lib", "-lQt6Quick3D", "-o", "test"
     system "./test"
   end
 end
